7e450a0bb616455e881b250e58387e6c0caef175
Thony <thony.lituba@edouardgand.fr>
2026-03-09 16:53:16 +0100

    ok

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace projet1;
+﻿using System.ComponentModel;
+
+namespace projet1;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+    }
+
+    
+}
+class Student
+{
+    private int id;
+    private string name;
+    private float average;
+    private bool isScholarshipHolder;
+    private Student(){}
+    private Student(string n, float a, bool s)
+    {
+        name = n;
+        average = a;
+        isScholarshipHolder = s;
     }
 }
