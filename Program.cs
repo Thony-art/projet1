704d3fc566ef6f41c6bfe68fa4e2bf7127c97d14
Thony <thony.lituba@edouardgand.fr>
2026-03-10 10:51:39 +0100

    Premier commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Student s1 = new Student("Alice", 15.5f, true);
+        Student s2 = new Student("Bernard", 4.0f, false);
+        Student s3 = new Student("Emma", 17.0f, true);
+        Student s4 = new Student("Lucas", 2.0f, false);
+        Student s5 = new Student("Sarah", 8.0f, false);
+
+        Course c1 = new Course("Mathematique", 3.0f, true);
+        Course c2=new Course("Informatique", 4.0f, true);
+        Course c3=new Course("Anglais", 2.0f, false);
+        Course c4 =new Course("Histoire", 3.0f, true);
+
+        c1.AddStudent(s1);
+        c2.AddStudent(s2);
+        c3.AddStudent(s3);
+        c4.AddStudent(s4);
+
+        List<Course> courses = new List<Course>();
+
+         courses.Add(c1);
+        courses.Add(c2);
+        courses.Add(c3);
+        courses.Add(c4);
+Console.WriteLine("Liste des cours :");
+
+foreach (Course c in courses)
+{
+    c.Display();
+}
+
+     
     }
 
     
@@ -17,11 +47,74 @@ class Student
     private string name;
     private float average;
     private bool isScholarshipHolder;
-    private Student(){}
-    private Student(string n, float a, bool s)
+    public Student(){}
+    public Student(string name, float average, bool isScholarshipHolder)
     {
-        name = n;
-        average = a;
-        isScholarshipHolder = s;
+        this.name = name;
+        this.average = average;
+        this.isScholarshipHolder = isScholarshipHolder;
     }
+    public void Display()
+{
+    Console.WriteLine("Nom : " + name);
+    Console.WriteLine("Moyenne : " + average);
+
+    if (isScholarshipHolder)
+        Console.WriteLine("Boursier");
+    else
+        Console.WriteLine("Non boursier");
+
+    Console.WriteLine();
+}
+}
+class Course
+{
+    private int id;
+    private string name;
+    private float credits;
+
+    private bool isMandatory;
+
+    private List<Student> student;
+
+    public Course() { }
+    public Course(string name, float credits, bool isMandatory)
+    {
+        this.name = name;
+        this.credits = credits;
+        this.isMandatory = isMandatory;
+        student = new List<Student>();
+    }
+    public void AddStudent(Student s)
+    {
+        student.Add(s);
+    }
+    
+    public void Display()
+{
+    Console.WriteLine("Nom du cours : " + name);
+    Console.WriteLine("Crédits : " + credits);
+
+    if (isMandatory)
+        Console.WriteLine("Cours obligatoire");
+    else
+        Console.WriteLine("Cours non obligatoire");
+
+    Console.WriteLine("Nombre d'étudiants inscrits : " + student.Count);
+    Console.WriteLine("---------------------------");
+}
+public void DisplayStudents()
+{
+    Console.WriteLine("Etudiants inscrits :");
+
+    foreach (Student s in student)
+    {
+        s.Display();
+    }
+}
+ 
+        
+    
+ 
+    
 }
